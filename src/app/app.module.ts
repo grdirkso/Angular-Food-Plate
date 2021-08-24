9c213009144d0148a01aaf8f1f8657fe695e6597
grdirkso <grdirkso@uark.edu>
2021-08-24 10:40:36 -0500

    main and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { HomeBtnComponent } from './home-btn/home-btn.component';
 
 
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, MainComponent, HomeBtnComponent],
   imports: [
     CommonModule, BrowserModule
   ],
