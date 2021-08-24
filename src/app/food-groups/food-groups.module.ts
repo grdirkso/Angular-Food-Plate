75b2fda222ea4e743539f42a84da5eae4234dfca
grdirkso <grdirkso@uark.edu>
2021-08-24 16:20:09 -0500

    directives

---
+++ src/app/food-groups/food-groups.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FoodGroupsComponent } from './food-groups.component';
+
+
+
+@NgModule({
+  declarations: [FoodGroupsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FoodGroupsModule { }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -7,7 +7,7 @@ import { UserStatusService } from './user-status.service';
 })
 export class UserService {
 
-  private user: User = new User(1, 'Kevin', 'M', '51+','M51+', {}, {fruitMet: false, vegMet: false, proteinMet: false, grainMet: false}, false, 'kevin@kevinruse.com');
+  private user: User = new User(1, 'Kevin', 'M', '51+','M51+', {}, {fruitMet: false, vegMet: true, proteinMet: false, grainMet: false}, false, 'kevin@kevinruse.com');
   constructor(
     @Optional() private userStatusService: UserStatusService
   ) {
