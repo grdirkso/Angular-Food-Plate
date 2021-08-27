c968a1726b38a6bd9c755f1d5370d8143b59d9be
grdirkso <grdirkso@uark.edu>
2021-08-27 14:39:05 -0500

    using user behavior subject

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../models/User';
 import { Router } from '@angular/router'
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'fp-main',
@@ -10,13 +11,28 @@ import { Router } from '@angular/router'
 export class MainComponent implements OnInit {
 
   router: Router;
-
-  @Input() user: User;
-  constructor(private _router: Router) {
+  currentUser: User;
+  routerLink: string;
+  
+  constructor(
+    private _router: Router,
+    private userService: UserService
+    ) {
     this.router = _router;
    }
 
   ngOnInit(): void {
+    this.userService.currentUser.subscribe(user => this.currentUser = user);
+  }
+
+  getRoute() {
+    if(!this.currentUser.registered){
+      this.routerLink = 'register';
+    } else if (this.currentUser.registered){
+      this.routerLink = 'myPlate'
+    } 
+
+    return this.routerLink
   }
 
 }
